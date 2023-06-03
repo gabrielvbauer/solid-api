e3c4ee170b4d4852c3f8e52a8f322757dc984d3b
Gabriel Bauer <gabriel.bauer9@hotmail.com>
2023-06-03 13:47:53 -0300

    feat: get user profile use case

--- src/repositories/in-memory/in-memory-users-repository.ts
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,11 +1,21 @@
-import { User, Prisma } from "@prisma/client";
-import { UsersRepository } from "../users-repository";
+import { User, Prisma } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
 
+  async findById(userId: string) {
+    const user = this.items.find((user) => user.id === userId)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
   async findByEmail(email: string) {
-    const user = this.items.find(item => item.email === email);
+    const user = this.items.find((item) => item.email === email)
 
     if (!user) {
       return null
@@ -20,12 +30,11 @@ export class InMemoryUsersRepository implements UsersRepository {
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
-      created_at: new Date()
+      created_at: new Date(),
     }
 
     this.items.push(user)
 
     return user
   }
-
 }
